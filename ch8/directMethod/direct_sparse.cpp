a10ac9e3a64735b1fba4574829245704c94e1613
kaka <hailongma61@gmail.com>
2018-06-19 08:20:43 +0800

    learn lk

--- ch8/directMethod/direct_sparse.cpp
+++ ch8/directMethod/direct_sparse.cpp
@@ -22,36 +22,37 @@ using namespace std;
 using namespace g2o;
 
 /********************************************
- * 本节演示了RGBD上的稀疏直接法 
+ * 本节演示了RGBD上的稀疏直接法
  ********************************************/
 
 // 一次测量的值，包括一个世界坐标系下三维点与一个灰度值
-struct Measurement
-{
-    Measurement ( Eigen::Vector3d p, float g ) : pos_world ( p ), grayscale ( g ) {}
+struct Measurement {
+    Measurement (Eigen::Vector3d p, float g): pos_world(p), grayscale(g) {}
     Eigen::Vector3d pos_world;
     float grayscale;
 };
 
-inline Eigen::Vector3d project2Dto3D ( int x, int y, int d, float fx, float fy, float cx, float cy, float scale )
+inline Eigen::Vector3d project2Dto3D(int x, int y, int d, float fx, float fy, float cx, float cy, float scale)
 {
-    float zz = float ( d ) /scale;
-    float xx = zz* ( x-cx ) /fx;
-    float yy = zz* ( y-cy ) /fy;
-    return Eigen::Vector3d ( xx, yy, zz );
+    float zz = float(d) / scale;
+    float xx = zz * (x - cx) / fx;
+    float yy = zz * (y - cy) / fy;
+    return Eigen::Vector3d (xx, yy, zz);
 }
 
-inline Eigen::Vector2d project3Dto2D ( float x, float y, float z, float fx, float fy, float cx, float cy )
+inline Eigen::Vector2d project3Dto2D(float x, float y, float z, float fx, float fy, float cx, float cy)
 {
-    float u = fx*x/z+cx;
-    float v = fy*y/z+cy;
-    return Eigen::Vector2d ( u,v );
+    float u = fx * x / z + cx;
+    float v = fy * y / z + cy;
+    return Eigen::Vector2d(u, v);
 }
 
 // 直接法估计位姿
 // 输入：测量值（空间点的灰度），新的灰度图，相机内参； 输出：相机位姿
 // 返回：true为成功，false失败
-bool poseEstimationDirect ( const vector<Measurement>& measurements, cv::Mat* gray, Eigen::Matrix3f& intrinsics, Eigen::Isometry3d& Tcw );
+bool poseEstimationDirect(const vector<Measurement>& measurements,
+                          cv::Mat* gray, Eigen::Matrix3f& intrinsics,
+                          Eigen::Isometry3d& Tcw);
 
 
 // project a 3d point into an image plane, the error is photometric error
@@ -156,16 +157,15 @@ public:
 
 int main ( int argc, char** argv )
 {
-    if ( argc != 2 )
-    {
-        cout<<"usage: useLK path_to_dataset"<<endl;
+    if (argc != 2) {
+        cout << "usage: useLK path_to_dataset" << endl;
         return 1;
     }
-    srand ( ( unsigned int ) time ( 0 ) );
+    srand((unsigned int)time(0));
     string path_to_dataset = argv[1];
     string associate_file = path_to_dataset + "/associate.txt";
 
-    ifstream fin ( associate_file );
+    ifstream fin(associate_file);
 
     string rgb_file, depth_file, time_rgb, time_depth;
     cv::Mat color, depth, gray;
@@ -177,49 +177,50 @@ int main ( int argc, char** argv )
     float fy = 519.0;
     float depth_scale = 1000.0;
     Eigen::Matrix3f K;
-    K<<fx,0.f,cx,0.f,fy,cy,0.f,0.f,1.0f;
+    K << fx, 0.f, cx, 0.f, fy, cy, 0.f, 0.f, 1.0f;
 
     Eigen::Isometry3d Tcw = Eigen::Isometry3d::Identity();
 
     cv::Mat prev_color;
     // 我们以第一个图像为参考，对后续图像和参考图像做直接法
-    for ( int index=0; index<10; index++ )
-    {
-        cout<<"*********** loop "<<index<<" ************"<<endl;
-        fin>>time_rgb>>rgb_file>>time_depth>>depth_file;
-        color = cv::imread ( path_to_dataset+"/"+rgb_file );
-        depth = cv::imread ( path_to_dataset+"/"+depth_file, -1 );
-        if ( color.data==nullptr || depth.data==nullptr )
-            continue; 
-        cv::cvtColor ( color, gray, cv::COLOR_BGR2GRAY );
-        if ( index ==0 )
-        {
+    for (int index = 0; index < 10; index++) {
+        cout << "*********** loop " << index << " ************" << endl;
+        fin >> time_rgb >> rgb_file >> time_depth >> depth_file;
+        color = cv::imread(path_to_dataset + "/" + rgb_file);
+        depth = cv::imread(path_to_dataset + "/" + depth_file, -1);
+        if (color.data == nullptr || depth.data == nullptr) {
+            continue;
+        }
+        cv::cvtColor(color, gray, cv::COLOR_BGR2GRAY);
+        if (index == 0) {
             // 对第一帧提取FAST特征点
             vector<cv::KeyPoint> keypoints;
             cv::Ptr<cv::FastFeatureDetector> detector = cv::FastFeatureDetector::create();
-            detector->detect ( color, keypoints );
-            for ( auto kp:keypoints )
-            {
+            detector->detect(color, keypoints);
+            for (auto kp : keypoints) {
                 // 去掉邻近边缘处的点
-                if ( kp.pt.x < 20 || kp.pt.y < 20 || ( kp.pt.x+20 ) >color.cols || ( kp.pt.y+20 ) >color.rows )
+                if (kp.pt.x < 20 || kp.pt.y < 20 || (kp.pt.x + 20) > color.cols || (kp.pt.y + 20) > color.rows) {
                     continue;
-                ushort d = depth.ptr<ushort> ( cvRound ( kp.pt.y ) ) [ cvRound ( kp.pt.x ) ];
-                if ( d==0 )
+                }
+                ushort d = depth.ptr<ushort>(cvRound(kp.pt.y))[cvRound(kp.pt.x)];
+                if (d == 0) {
                     continue;
-                Eigen::Vector3d p3d = project2Dto3D ( kp.pt.x, kp.pt.y, d, fx, fy, cx, cy, depth_scale );
-                float grayscale = float ( gray.ptr<uchar> ( cvRound ( kp.pt.y ) ) [ cvRound ( kp.pt.x ) ] );
-                measurements.push_back ( Measurement ( p3d, grayscale ) );
+                }
+                // 相机坐标系到世界坐标系
+                Eigen::Vector3d p3d = project2Dto3D(kp.pt.x, kp.pt.y, d, fx, fy, cx, cy, depth_scale);
+                float grayscale = float(gray.ptr<uchar>(cvRound(kp.pt.y))[cvRound(kp.pt.x)]);
+                measurements.push_back(Measurement(p3d, grayscale));
             }
             prev_color = color.clone();
             continue;
         }
         // 使用直接法计算相机运动
         chrono::steady_clock::time_point t1 = chrono::steady_clock::now();
-        poseEstimationDirect ( measurements, &gray, K, Tcw );
+        poseEstimationDirect(measurements, &gray, K, Tcw);
         chrono::steady_clock::time_point t2 = chrono::steady_clock::now();
-        chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>> ( t2-t1 );
-        cout<<"direct method costs time: "<<time_used.count() <<" seconds."<<endl;
-        cout<<"Tcw="<<Tcw.matrix() <<endl;
+        chrono::duration<double> time_used = chrono::duration_cast<chrono::duration<double>>(t2 - t1);
+        cout << "direct method costs time: "<< time_used.count() << " seconds." << endl;
+        cout << "Tcw=" << Tcw.matrix() << endl;
 
         // plot the feature points
         cv::Mat img_show ( color.rows*2, color.cols, CV_8UC3 );
@@ -250,12 +251,13 @@ int main ( int argc, char** argv )
     return 0;
 }
 
-bool poseEstimationDirect ( const vector< Measurement >& measurements, cv::Mat* gray, Eigen::Matrix3f& K, Eigen::Isometry3d& Tcw )
-{
+bool poseEstimationDirect(const vector<Measurement>& measurements,
+                          cv::Mat* gray, Eigen::Matrix3f& intrinsics,
+                          Eigen::Isometry3d& Tcw) {
     // 初始化g2o
-    typedef g2o::BlockSolver<g2o::BlockSolverTraits<6,1>> DirectBlock;  // 求解的向量是6＊1的
-    DirectBlock::LinearSolverType* linearSolver = new g2o::LinearSolverDense< DirectBlock::PoseMatrixType > ();
-    DirectBlock* solver_ptr = new DirectBlock ( linearSolver );
+    typedef g2o::BlockSolver<g2o::BlockSolverTraits<6, 1>> DirectBlock;  // 求解的向量是6＊1的
+    DirectBlock::LinearSolverType* linearSolver = new g2o::LinearSolverDense<DirectBlock::PoseMatrixType>();
+    DirectBlock* solver_ptr = new DirectBlock(linearSolver);
     // g2o::OptimizationAlgorithmGaussNewton* solver = new g2o::OptimizationAlgorithmGaussNewton( solver_ptr ); // G-N
     g2o::OptimizationAlgorithmLevenberg* solver = new g2o::OptimizationAlgorithmLevenberg ( solver_ptr ); // L-M
     g2o::SparseOptimizer optimizer;
@@ -286,4 +288,3 @@ bool poseEstimationDirect ( const vector< Measurement >& measurements, cv::Mat* 
     optimizer.optimize ( 30 );
     Tcw = pose->estimate();
 }
-
