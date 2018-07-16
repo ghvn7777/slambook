0cbcae04669753e3fdabea89b581faed87b8bad0
kaka <hailongma61@gmail.com>
2018-07-16 13:05:43 +0800

    loop closure complete

--- ch12/loop_closure.cpp
+++ ch12/loop_closure.cpp
@@ -15,8 +15,8 @@ using namespace std;
 int main(int argc, char** argv) {
     // read the images and database  
     cout << "reading database" << endl;
-    DBoW3::Vocabulary vocab("./vocabulary.yml.gz");
-    // DBoW3::Vocabulary vocab("./vocab_larger.yml.gz");  // use large vocab if you want: 
+    // DBoW3::Vocabulary vocab("./vocabulary.yml.gz");
+    DBoW3::Vocabulary vocab("./vocab_larger.yml.gz");  // use large vocab if you want:
     if (vocab.empty()) {
         cerr << "Vocabulary does not exist." << endl;
         return 1;
@@ -43,31 +43,29 @@ int main(int argc, char** argv) {
     // we can compare the images directly or we can compare one image to a database 
     // images :
     cout << "comparing images with images " << endl;
-    for (int i = 0; i<images.size(); i++)
-    {
+    for (int i = 0; i < images.size(); i++) {
         DBoW3::BowVector v1;
-        vocab.transform( descriptors[i], v1 );
-        for ( int j=i; j<images.size(); j++ )
-        {
+        vocab.transform(descriptors[i], v1);
+        for (int j = i; j < images.size(); j++) {
             DBoW3::BowVector v2;
-            vocab.transform( descriptors[j], v2 );
+            vocab.transform(descriptors[j], v2);
             double score = vocab.score(v1, v2);
-            cout<<"image "<<i<<" vs image "<<j<<" : "<<score<<endl;
+            cout << "image " << i << " vs image " << j << " : " << score << endl;
         }
         cout<<endl;
     }
     
     // or with database 
-    cout<<"comparing images with database "<<endl;
-    DBoW3::Database db( vocab, false, 0);
-    for ( int i=0; i<descriptors.size(); i++ )
-        db.add(descriptors[i]);
-    cout<<"database info: "<<db<<endl;
-    for ( int i=0; i<descriptors.size(); i++ )
-    {
+    cout << "comparing images with database " << endl;
+    DBoW3::Database db(vocab, false, 0);
+    for (const auto &descriptor : descriptors) {
+        db.add(descriptor);
+    }
+    cout << "database info: " << db << endl;
+    for (int i = 0; i < descriptors.size(); i++) {
         DBoW3::QueryResults ret;
-        db.query( descriptors[i], ret, 4);      // max result=4
-        cout<<"searching for image "<<i<<" returns "<<ret<<endl<<endl;
+        db.query(descriptors[i], ret, 4);      // max result=4
+        cout << "searching for image " << i << " returns " << ret << endl << endl;
     }
     cout<<"done."<<endl;
 }
