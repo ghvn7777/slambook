5420aff3413fbb058d55c1508cf46cce1471e7cd
kaka <hailongma61@gmail.com>
2019-05-07 22:44:23 +0800

    join map

--- ch5/joinMap/joinMap.cpp
+++ ch5/joinMap/joinMap.cpp
@@ -1,74 +1,73 @@
 #include <iostream>
 #include <fstream>
-using namespace std;
+
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
-#include <Eigen/Geometry> 
+#include <Eigen/Geometry>
 #include <boost/format.hpp>  // for formating strings
-#include <pcl/point_types.h> 
-#include <pcl/io/pcd_io.h> 
+#include <pcl/point_types.h>
+#include <pcl/io/pcd_io.h>
 #include <pcl/visualization/pcl_visualizer.h>
 
-int main( int argc, char** argv )
-{
+using namespace std;
+
+int main(int argc, char** argv) {
     vector<cv::Mat> colorImgs, depthImgs;    // 彩色图和深度图
     vector<Eigen::Isometry3d, Eigen::aligned_allocator<Eigen::Isometry3d>> poses;         // 相机位姿
-    
+
     ifstream fin("./pose.txt");
-    if (!fin)
-    {
-        cerr << "请在有pose.txt的目录下运行此程序" << endl;
+    if (!fin) {
+        cerr << "请在有 pose.txt 的目录下运行此程序" << endl;
         return 1;
     }
-    
-    for (int i = 0; i < 5; i++)
-    {
+
+    for (int i = 0; i < 5; i++) {
         boost::format fmt("./%s/%d.%s"); //图像文件格式
-        colorImgs.push_back(cv::imread((fmt % "color" % (i+1) % "png").str()));
-        depthImgs.push_back(cv::imread((fmt % "depth" % (i+1) % "pgm").str(), -1)); // 使用-1读取原始图像
-        
+        colorImgs.push_back(cv::imread((fmt % "color" % (i + 1) % "png").str()));
+        depthImgs.push_back(cv::imread((fmt % "depth" % (i + 1) % "pgm").str(), -1)); // 使用-1读取原始图像
+
         double data[7] = {0};
-        for (auto& d : data)
+        for (auto& d : data) {
             fin >> d;
+        }
+
         Eigen::Quaterniond q(data[6], data[3], data[4], data[5]);
         Eigen::Isometry3d T(q);
         T.pretranslate(Eigen::Vector3d(data[0], data[1], data[2]));
         poses.push_back(T);
     }
-    
+
     // 计算点云并拼接
-    // 相机内参 
+    // 相机内参
     double cx = 325.5; // 到原点平移的像素
-    double cy = 253.5; 
+    double cy = 253.5;
     double fx = 518.0; // fx = alpha * f 是焦距乘以到像素坐标系的缩放，单位也是像素
     double fy = 519.0;
     double depthScale = 1000.0;
-    
+
     cout << "正在将图像转换为点云..." << endl;
-    
+
     // 定义点云使用的格式：这里用的是XYZRGB
-    typedef pcl::PointXYZRGB PointT; 
+    typedef pcl::PointXYZRGB PointT;
     typedef pcl::PointCloud<PointT> PointCloud;
-    
+
     // 新建一个点云
-    PointCloud::Ptr pointCloud(new PointCloud); 
-    for (int i = 0; i < 5; i++)
-    {
-        cout << "转换第 " << i + 1 << " 张图像中" << endl; 
-        cv::Mat color = colorImgs[i]; 
+    PointCloud::Ptr pointCloud(new PointCloud);
+    for (int i = 0; i < 5; i++) {
+        cout << "转换第 " << i + 1 << " 张图像中" << endl;
+        cv::Mat color = colorImgs[i];
         cv::Mat depth = depthImgs[i];
         Eigen::Isometry3d T = poses[i];
-        for (int v = 0; v < color.rows; v++)
-            for (int u = 0; u < color.cols; u++)
-            {
+        for (int v = 0; v < color.rows; v++) {
+            for (int u = 0; u < color.cols; u++) {
                 unsigned int d = depth.ptr<unsigned short>(v)[u]; // 深度值
                 if (d == 0) continue; // 为0表示没有测量到
-                Eigen::Vector3d point; 
+                Eigen::Vector3d point;
                 point[2] = double(d) / depthScale; // 除以深度的缩放倍数，得到距离障碍物多少米
                 point[0] = (u - cx) * point[2] / fx; // P_x = (u - cx) * Z / fx 得到该点相机观测坐标 P_x
                 point[1] = (v - cy) * point[2] / fy; // P_y = (u - cy) * Z / fy 得到该点相机观测坐标 P_y
-                Eigen::Vector3d pointWorld = T * point;
-                
+                Eigen::Vector3d pointWorld = T * point; // 相机坐标系转到世界坐标系
+
                 PointT p ;
                 p.x = pointWorld[0];
                 p.y = pointWorld[1];
@@ -78,8 +77,9 @@ int main( int argc, char** argv )
                 p.r = color.data[v * color.step + u * color.channels() + 2];
                 pointCloud->points.push_back(p);
             }
+        }
     }
-    
+
     pointCloud->is_dense = false;
     cout << "点云共有 " << pointCloud->size() << " 个点." <<endl;
     pcl::io::savePCDFileBinary("map.pcd", *pointCloud);
